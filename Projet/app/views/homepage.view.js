7d21986b71d4cb3cdd7e7c245fa8c7e67f6d7a15
nicolas.aubert1 <nicolas.aubert1@hes-so.ch>
2022-06-08 16:06:23 +0200

    Updating Homepage informations

--- Projet/app/views/homepage.view.js
+++ Projet/app/views/homepage.view.js
@@ -6,8 +6,12 @@
     constructor()
     {
       this.app = Helper.getElement('#root');
-      this.title = Helper.createElement('h1', "mt-5 mb-3");
-      this.title.textContent = 'Home';
+
+      this.informationTitle = Helper.createElement('h1', "text-center mt-5 mb-3");
+      this.informationTitle.innerHTML = "Welcome to <i>SandwichMaker</i> !"
+
+      this.informationText = Helper.createElement('h5', "text-center mb-5");
+      this.informationText.textContent = "Here you can create, manage and command your own personalized sandwiches."
 
       this.createButton = Helper.createElement('button', 'btn btn-info text-light m-3');
       this.createButton.textContent = 'Start creating a sandwich';
@@ -19,6 +23,6 @@
         Helper.redirect('create_sandwich');
       });
       
-      this.app.append(this.title,this.createButton);
+      this.app.append(this.informationTitle, this.informationText, this.createButton);
   }
 }
