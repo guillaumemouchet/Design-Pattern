9b4c153ce7b18be5018bff6200e77644e3956ffe
nicolas.aubert1 <nicolas.aubert1@hes-so.ch>
2022-06-08 19:06:54 +0200

    Updating About view

--- Projet/app/views/about.view.js
+++ Projet/app/views/about.view.js
@@ -7,9 +7,9 @@
     {
       this.app = Helper.getElement('#root');
       this.title = Helper.createElement('h1', "mt-5 mb-3");
-      this.title.textContent = 'Home';
-      this.text = Helper.createElement('p', "mt-5 mb-3");
-      this.text.textContent = 'Ce site web a pour but de symboliser le choix que ferait un utilisateur dans un fast-food Subway, l\'utilisateur peut choisir différents ingrédients dans son sandwich et peut ajouter plusieurs sandwich a son panier, une fois la commande pasée, l\'utilisateur attend un peu avant de recevoir une confirmation comme quoi ces sandwich sont prêt.';
+      this.title.textContent = 'About';
+      this.text = Helper.createElement('p', "mb-3 text-justify");
+      this.text.textContent = 'Ce site web a pour but de symboliser le choix que ferait un utilisateur dans un fast-food Subway, l\'utilisateur peut choisir différents ingrédients dans son sandwich et peut ajouter plusieurs sandwichs à son panier. Il peut ensuite passer commande, l\'utilisateur devra alors attendre un peu de temps avant de recevoir une confirmation, puis sera redirigé sur la page d\'accueil';
       this.createButton = Helper.createElement('button', 'btn btn-info text-light m-3');
       this.createButton.textContent = 'Start creating a sandwich';
 
